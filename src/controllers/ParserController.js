368bf699d5a0d51e38f8562f3bc4db885c2cab19
vitu-77 <marcelo.victor05@gmail.com>
2020-06-04 21:41:59 -0300

    Add: separate the log in each game with total_kills

--- src/controllers/ParserController.js
+++ src/controllers/ParserController.js
@@ -4,13 +4,15 @@ module.exports = {
 	index: async (req, res) => {
 		try {
 			const log = await Parser.readFile();
-            console.log(log)
+			const games = await Parser.groupByGames(log);
+// console.log(games)
 			return res.status(200).json({
 				// log: log.toString(),
-				sucess: 'OK',
+                sucess: 'OK',
+                games
 			});
 		} catch (error) {
-			console.log(error);
+			// console.log(error);
 			return res.status(error.status || 500).json(...error);
 		}
 	},
--- src/helpers/Parser.js
+++ src/helpers/Parser.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
+const shortid = require('shortid');
+const readLine = require('readline');
+
 module.exports = {
 	readFile: async () => {
 		const log = await fs.readFileSync(
@@ -9,8 +12,56 @@ module.exports = {
 			(error, data) => {
 				if (error) throw error;
 			}
-        );
-        
-        return log;
-    },
+		);
+
+		return log.toString();
+	},
+
+	groupByGames: async (log) => {
+		try {
+			const fileStream = fs.createReadStream(path.resolve(__dirname, '..', 'log', 'quake.log'));
+
+			const _readline = readLine.createInterface({
+				input: fileStream,
+			});
+
+			const games = [];
+
+			const buildGame = async (line) => {
+				let kills = 0;
+				const id = shortid.generate();
+				const players = [];
+
+				if (line.includes('InitGame')) {
+					games.push({
+						id,
+						players,
+						total_kills: kills,
+					});
+				}
+
+				if (line.includes('Kill:')) {
+					games[games.length - 1].total_kills = games[games.length - 1].total_kills + 1;
+				}
+			};
+
+			for await (const line of _readline) {
+				buildGame(line);
+			}
+
+			// for await (const line of readline) {
+			// 	if (line.includes('InitGame')) {
+			//         console.log(line)
+			// 		g++;
+			// 	}
+			// 	if (line.includes('Kill:')) {
+			// 		k++;
+			// 	}
+			// }
+
+			return { games };
+		} catch (error) {
+			return error;
+		}
+	},
 };
